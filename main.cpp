255176d3fc25d18a494ea8a43cac143bd721ef70
CDRPico <cramirezp@alumnos.uai.cl>
2020-10-30 09:18:34 -0300

    main.cpp update

--- main.cpp
+++ main.cpp
@@ -2,30 +2,95 @@
 #include"inc/GAPM.h"
 #include"inc/SFLP_GAPM.h"
 #include"inc/BendersAPM_SFLP.h"
+#include"inc/OuterBendersSFLP.h"
 #include<fstream>
+#include"inc/CapPlan_GAPM.h"
+#include"SMPS_ElecPlan.h"
+#include"inc/BendersAPM_CP.h"
+
 using namespace std;
 
 int main(int argc, char **argv) {
-
-	//string file = argv[1];
-	string file = "cap111.txt";
+	//string file = argv[1];	
 	//string file_stoch = argv[2];
-	string file_stoch = "cap111_0.20_1500.txt";
 	//const char algo = argv[3][0];
-	const char algo = 'a';
 
-	if (algo == 'n') {
-		//Creating instance of the problem based on previous files
-		SFLP_GAPM Instance(file, file_stoch);
-		//Start the GAPM
-		GAPM algor;
-		algor.gapm_algorithm(Instance,algo);
+	//problem, 1 for Capacity planning
+	//problem, 2 for facility location
+	int problem = 1;
+
+	if (problem == 1) {
+		string file_ce_time = "time_nospace.mps";
+		string file_ce_core = "core_nospace.mps";
+		string file_ce_stoch = "stoch_nospace.mps";
+		size_t nnn = 1000;
+		//testin.generate_instances(nnn);
+		string testname = "capplan_small_10000.txt";
+		const char algo = 'm';
+		if (algo == 'n') {
+			//Creating instance of the problem based on previous files
+			CP_GAPM Instance(file_ce_core, file_ce_stoch, file_ce_time, testname);
+			Instance.gapm_type = 's';
+			//Instance.partition.push_back(vector<size_t>(create_partition(Instance.nScenarios)));
+			/*Instance.partition.clear();
+			Instance.partition.resize(Instance.nScenarios);
+			for (size_t s = 0; s < Instance.nScenarios; s++) {
+				Instance.partition.push_back({ s });
+			}*/
+			//Start the GAPM
+			GAPM algor;
+			algor.gapm_algorithm(Instance, algo, Instance.stoch_constr.size());
+		}
+		else if (algo == 'f') {
+			CP_GAPM Instance(file_ce_core, file_ce_stoch, file_ce_time, testname);
+			Instance.gapm_type = 's';
+			Instance.FullCP();
+		}
+		else {
+			//Initialize benders
+			BendersCP Instance(file_ce_core, file_ce_stoch, file_ce_time, testname);
+			Instance.gapm_type = 's';
+			vector<vector<size_t>> part;
+			//part.resize(Instance.nScenarios);
+			part.push_back(vector<size_t>(create_partition(Instance.nScenarios)));
+			vector<double> xb(Instance.nFacilities, 0.0);
+			//Create and run procedure
+			double optimal = Instance.runBenders(algo, part, xb);
+			//Instance.IterativeMaster(algo, file, file_stoch, part);
+		}
 	}
-	else {
-		//Initialize benders
-		BendersSFLP Instance(file, file_stoch);
-		//Create and run procedure
-		Instance.CreateMaster(algo);
+	else if (problem == 2) {
+		string file = "cap111.txt";
+		string file_stoch = "cap111_0.10_250.txt";
+		const char algo = 'n';
+		if (algo == 'n') {
+			//Creating instance of the problem based on previous files
+			SFLP_GAPM Instance(file, file_stoch);
+			Instance.gapm_type = 's';
+			//Start the GAPM
+			GAPM algor;
+			algor.gapm_algorithm(Instance, algo, Instance.nClients);
+		}
+		else {
+			if (algo == 'o' || algo == 'l') {
+				//Creating instance of the problem based on previous files
+				OuterBendersSFLP Instance(file, file_stoch);
+				//Start the GAPM
+				GAPM algor;
+				algor.gapm_algorithm(Instance, algo, Instance.nClients);
+			}
+			else {
+				//Initialize benders
+				BendersSFLP Instance(file, file_stoch);
+				Instance.gapm_type = 's';
+				vector<vector<size_t>> part;
+				part.push_back(vector<size_t>(create_partition(Instance.nScenarios)));
+				vector<double> xb(Instance.nFacilities, 0.0);
+				//Create and run procedure
+				double optimal = Instance.CreateMaster(algo, part, xb);
+				//Instance.IterativeMaster(algo, file, file_stoch, part);
+			}
+		}
 	}
 	cin.get();
 }
